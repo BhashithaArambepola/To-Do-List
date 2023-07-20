4a16ca533c1f8ef1d385380170ac3169a353ff7b
Bhashitha Arambepola <barambepola@gmail.com>
2023-07-20 12:30:35 +0530

    setUp File Structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AppBody from './Component/AppBody';
 
 function App() {
   return (
     <div>
-      
+      <AppBody/>
     </div>
   );
 }
---
+++ src/Component/AppBody.js
@@ -0,0 +1,21 @@
+
+import React ,{useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleCheck, faPen, faTrashCan} from '@fortawesome/free-solid-svg-icons'
+import '../CSS/AppBody.css'
+
+function AppBody() {
+
+
+
+
+  return (
+    <div className='appBody py-5'>
+<h1>To Do List</h1>
+
+    </div>
+  )
+}
+
+export default AppBody
