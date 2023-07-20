4fb354906be349bd658593debf405df29ce638c8
Bhashitha Arambepola <barambepola@gmail.com>
2023-07-20 16:19:48 +0530

    create UI

--- src/Component/AppBody.js
+++ src/Component/AppBody.js
@@ -9,8 +9,8 @@ function AppBody() {
 
 // toDo list State set
 const [toDo, setToDo]=useState([
-    {"id":1, "title":"task 1","status":false},
-    {"id":2, "title":"task 2","status":false}
+    {"id":1, "title":"Task 1","status":false},
+    {"id":2, "title":"Task 2","status":false}
 ])
 
 // temp state
@@ -47,8 +47,37 @@ const updateTask=()=>{
 
 
 return (
-    <div className='appBody py-5'>
-<h1>To Do List</h1>
+    <div className='appBody py-5 container'>
+<h1 className='py-5'>To Do List</h1>
+
+
+{/* Display To-Dos */}
+{toDo && toDo.length ? '': 'No Tasks...'}
+
+
+{toDo && toDo.sort((a,b)=>a.id>b.id ? 1:-1)
+.map((task,index)=>{
+    return(
+        <React.Fragment key={task.id}>
+
+
+<div className='col taskBg p-3 
+mb-3 rounded text-start position-relative d-flex justify-content-between'>
+<div className={task.status? 'done':''}>
+<span className='taskNumber d-flex text-center align-items-center justify-content-center me-3 '>{index+1}</span>
+<span className='taskText'>{task.title}</span>
+</div>
+<div className='iconWrap'>
+    <span title='Add New' className='px-2'><FontAwesomeIcon icon={faCircleCheck}/></span>
+    <span title='Edit TAsk' className='px-2'><FontAwesomeIcon icon={faPen}/></span>
+    <span title='Delete' className='px-2'><FontAwesomeIcon icon={faTrashCan}/></span>
+</div>
+</div>
+
+
+        </React.Fragment>
+    )
+})}
 
     </div>
   )
